66704999ba7ad1d29b30c8c9a53a78c354d4d3f7
murilodpl <inayievmpl@gmail.com>
2021-09-29 17:15:29 -0300

    First commit

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    Home
+    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">click me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  setup() {
+    console.log("setup");
+
+    let name = "Mario";
+    let age = 30;
+
+    const handleClick = () => {
+      console.log("You clicked me");
+    };
+
+    return {
+      name,
+      age,
+      handleClick,
+    };
+  },
+};
+</script>
