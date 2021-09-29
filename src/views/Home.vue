2fd97c8d370516936c5259dbf4193fab71e54192
murilodpl <inayievmpl@gmail.com>
2021-09-29 17:52:10 -0300

    Update page Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,20 +3,25 @@
     Home
     <p>My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">click me</button>
+    <button @click="age++">add 1 to age</button>
+    <input type="text" v-model="name">
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
   name: "Home",
   setup() {
-    console.log("setup");
+    // const p = ref(null);
 
-    let name = "Mario";
-    let age = 30;
+    const name = ref("Mario");
+    const age = ref(30);
 
     const handleClick = () => {
-      console.log("You clicked me");
+      name.value = "Luigi";
+      age.value = 35;
     };
 
     return {
