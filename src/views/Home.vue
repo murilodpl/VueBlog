79cef642251cbdffcf73bebf246dcea518f56b98
murilodpl <inayievmpl@gmail.com>
2021-09-30 17:58:59 -0300

    Comparing REF and REACTIVE

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,44 @@
 <template>
   <div class="home">
-    Home
-    <p>My name is {{ name }} and my age is {{ age }}</p>
-    <button @click="handleClick">click me</button>
-    <button @click="age++">add 1 to age</button>
-    <input type="text" v-model="name">
+    <h1>Home</h1>
+
+    <h2>Refs</h2>
+    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
+    <button @click="updateNinjaOne">Update ninja one</button>
+
+    <h2>Reactive</h2>
+    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
+    <button @click="updateNinjaTwo">Update ninja two</button>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, reactive } from "vue";
 
 export default {
   name: "Home",
   setup() {
     // const p = ref(null);
 
-    const name = ref("Mario");
-    const age = ref(30);
+    const ninjaOne = ref({ name: "Mario", age: 30 });
+    const ninjaTwo = reactive({ name: "Luigi", age: 35 });
 
-    const handleClick = () => {
-      name.value = "Luigi";
-      age.value = 35;
+    const nameOne = ref("Mario");
+    const nameTwo = reactive("Luigi"); //Not reactive bc is a primitive value
+
+    const updateNinjaOne = () => {
+      ninjaOne.value.age = 40;
+    };
+
+    const updateNinjaTwo = () => {
+      ninjaTwo.age = 45;
     };
 
     return {
-      name,
-      age,
-      handleClick,
+      ninjaOne,
+      ninjaTwo,
+      updateNinjaOne,
+      updateNinjaTwo,
     };
   },
 };
